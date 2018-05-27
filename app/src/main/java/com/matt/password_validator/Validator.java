52259a189dfa492f19cf75e3a444d06c2f584021
Matt <mmacmul2@dal.ca>
2018-05-26 22:29:14 -0300

    Initial commit

---
+++ app/src/main/java/com/matt/password_validator/Validator.java
@@ -0,0 +1,25 @@
+package com.matt.password_validator;
+
+public class Validator {
+
+    static public int validate(String password){
+       int testsPassed = 0;
+
+
+
+       return testsPassed;
+    }
+
+    static public boolean passwordCheck(String password){
+        boolean check = false;
+
+        return check;
+    }
+
+    static public boolean lengthCheck(String password){
+        boolean check = false;
+
+        return check;
+    }
+
+}
---
+++ app/src/test/java/com/matt/password_validator/ValidatorTest.java
@@ -0,0 +1,38 @@
+package com.matt.password_validator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ValidatorTest {
+    @Test
+    public void testPasswordNotAllowed() {
+        String pass1 = "abcdefg";
+        String pass2 = "password";
+
+        assertTrue(Validator.passwordCheck(pass1));
+        assertFalse(Validator.passwordCheck(pass2));
+    }
+    @Test
+    public void testLength() {
+        String pass1 = "xyz";
+        String pass2 = "abcdefgh";
+        String pass3 = "ijklmnopwqrstuv";
+
+        assertFalse(Validator.lengthCheck(pass1));
+        assertTrue(Validator.lengthCheck(pass2));
+        assertTrue(Validator.lengthCheck(pass3));
+    }
+
+    @Test
+    public void testValidate() {
+        String pass1 = "password";
+        String pass2 = "abcdef";
+        String pass3 = "ijklmnopwq";
+
+       assertEquals(0, Validator.validate(pass1));
+       assertEquals(1, Validator.validate(pass2));
+       assertEquals(2, Validator.validate(pass3));
+    }
+}
